fb927e1b2e6b75b66b578a1e94ce326fc33ccd42
Tomiris Martbatova <unicorn@Tomiris-MacBook-Air.local>
2025-08-26 05:50:54 +0300

    Geri dön butonu konumu düzeltildi - menü ile çakışma sorunu çözüldü

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -29,6 +29,11 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/about.js"
+    ],
+    "/services": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/services.js"
     ]
   },
   "ampFirstPages": []
